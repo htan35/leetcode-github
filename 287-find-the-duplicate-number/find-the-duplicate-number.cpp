6fb7d04c3fd4f1e10a652b7eac3efe42775c9b55
Harsh Tanwar <156991396+htan35@users.noreply.github.com>
2026-08-01 03:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 64.9 MB (99.61%) - PUSH IT MAN!

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,16 +1,41 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         map<int,int> mpp;
+
+//         for(int i = 0; i < nums.size(); i++){
+//             mpp[nums[i]]++;
+//         }
+
+//         for(auto it : mpp){
+//             if(it.second > 1) return it.first;
+//         }
+
+//         return -1; 
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int,int> mpp;
+        int slow = 0;
+        int fast = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast)
+                break;
         }
 
-        for(auto it : mpp){
-            if(it.second > 1) return it.first;
+        slow = 0;
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1; 
+        return slow;
     }
 };
