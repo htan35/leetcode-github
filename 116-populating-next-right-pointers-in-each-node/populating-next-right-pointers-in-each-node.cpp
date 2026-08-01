546b71daafec3d2558a2839ddaa07131a25e8630
Harsh Tanwar <156991396+htan35@users.noreply.github.com>
2026-08-01 17:05:43 +0530

    Time: 7 ms (97.52%) | Memory: 19 MB (82.90%) - PUSH IT MAN!

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -16,41 +16,74 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         queue<Node*> q;
+        
+//         vector<vector<Node*>> ans;
+//         if(root == NULL) return NULL;
+
+//         q.push(root);
+//         while(!q.empty()){
+//             int size = q.size();
+//             vector<Node*> level;
+            
+//             for(int i=0; i<size;i++){
+//                 Node* node = q.front();
+//                 q.pop();
+//                 if(node->left != NULL) q.push(node->left);
+//                 if(node->right != NULL) q.push(node->right);
+
+//                 level.push_back(node);
+//             }
+//             ans.push_back(level);
+//         }
+
+//         for(int i=0; i<ans.size();i++){
+//             for(int j=0;j<ans[i].size();j++){
+//                 if(j == ans[i].size() -1){
+//                     ans[i][j]->next = NULL;
+//                 }
+//                 else{
+//                     ans[i][j]->next = ans[i][j+1];
+//                 }
+
+//             }
+//         }
+//     return ans[0][0];
+//     }
+// };
+
 class Solution {
 public:
     Node* connect(Node* root) {
-        queue<Node*> q;
-        
-        vector<vector<Node*>> ans;
-        if(root == NULL) return NULL;
+        if (!root) return nullptr;
 
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<Node*> level;
-            
-            for(int i=0; i<size;i++){
-                Node* node = q.front();
-                q.pop();
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
+        Node* curr = root;
 
-                level.push_back(node);
-            }
-            ans.push_back(level);
-        }
+        // Traverse down level by level
+        while (curr->left) {
+            Node* temp = curr;
 
-        for(int i=0; i<ans.size();i++){
-            for(int j=0;j<ans[i].size();j++){
-                if(j == ans[i].size() -1){
-                    ans[i][j]->next = NULL;
-                }
-                else{
-                    ans[i][j]->next = ans[i][j+1];
+            // Traverse horizontally across the current level
+            while (temp) {
+                // 1. Connect left child -> right child
+                temp->left->next = temp->right;
+
+                // 2. Connect right child -> adjacent left child (across subtrees)
+                if (temp->next) {
+                    temp->right->next = temp->next->left;
                 }
 
+                // Move to next node in current level
+                temp = temp->next;
             }
+
+            // Move to the leftmost node of the next level
+            curr = curr->left;
         }
-    return ans[0][0];
+
+        return root;
     }
 };
